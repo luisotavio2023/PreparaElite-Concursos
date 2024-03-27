21be321640c4b2065dd313cf969fb031def64493
luisotavio2023 <luisogois@gmail.com>
2024-03-27 17:23:43 -0300

    Melhorando a classe usuário

--- Model/Usuarios.php
+++ Model/Usuarios.php
@@ -3,15 +3,23 @@
   class Usuarios{
 
     //Atributos
+    private int $id;
+    private string $email;
+    private string $senha;
     private string $nome;
+    private string $tipous;
     private $dataNascimento;
     private string $descricao;
     private string $nacionalidade;
   
 
        // Construtor da classe
-       public function __construct($Nome, $Nacionalidade, $Formacao, $dataNascimento) {
+       public function __construct($Id, $Email, $Senha, $Nome, $TipoUs, $Nacionalidade, $Formacao, $dataNascimento) {
+        $this->id = $Id;
+        $this->email = $Email;
+        $this->senha = $Senha;
         $this->nome = $Nome;
+        $this->tipous = $TipoUs;
         $this->nacionalidade = $Nacionalidade;
         $this->formacao = $Formacao;
         $this->dataNascimento = $dataNascimento;
