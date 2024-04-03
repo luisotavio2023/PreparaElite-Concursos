235cd72135b20e1dc2a96a97d1aa6eeee77ff378
luisotavio2023 <luisogois@gmail.com>
2024-04-03 17:41:07 -0300

    Ajustando o código

--- Model/Produto.php
+++ Model/Produto.php
@@ -19,6 +19,20 @@ require "BancodeDados.php";
         $this->valor = $Valor;
         $this->quantidadeprod = $Quantidadeprod;
     }
+
+    public static function getAll() {
+        $conn = require('../config/database.php');
+        $sql = "SELECT nome, descricao, quantidade_disponivel, valor, imagem FROM produtos";
+        $resultado = $conexao->query($sql);
+        $produtos = [];
+        if ($resultado->num_rows > 0) {
+            while ($produto = $resultado->fetch_assoc()) {
+                $produtos[] = $produto;
+            }
+        }
+        $conexao->close();
+        return $produtos;
+    }
  }
 
 
