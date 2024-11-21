1dcf6217e26d4825bcdc93cf4fb08b56ea43fa81
Luis Otávio <luisogois@gmail.com>
2024-11-20 23:13:28 -0300

    Adicionando tela gerenciamento geral

---
+++ View/grcgeral.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="PT-BR">
+  <head>
+    <title>Gerenciamento Geral - PreparaElite</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg" style="background-color: #004aad;">
+        <a class="navbar-brand text-white" href="#">PreparaElite Concursos</a>
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Perfil
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Gerenciar Perfil</a>
+              <a class="dropdown-item" href="#">Carrinho</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Sair/logout</a>
+            </div>
+          </li>
+        </ul>
+    </nav>
+
+    <div class="d-flex justify-content-center align-items-center">
+        <h1>Gerenciamento Geral</h1>
+    </div>
+
+    <div>
+        <div>
+
+        </div>
+    </div>
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
